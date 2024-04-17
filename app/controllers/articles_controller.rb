f6b8aa3e5c53ca6d825f584a67fcda2ed8b20dc5
田口　勝貴 <d0811tgc@gmail.com>
2024-04-18 00:17:11 +0900

    1.記事投稿機能作成 ・create処理 ・データ保存 ・プライベートメソッド 	入力データ取得 2.記事一覧ページの記事作成リンク作成 3.ホーム画面ルーティング設定 root 4.新規投稿フォーム作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     # binding.pry
   end
+  def new
+    binding.pry
+    @article = Article.new
+    binding.pry
+  end
   def create
-    # binding.pry
-    @article = Article.new(
-      title:params[:title],
-      content:params[:content]
-    )
-
+    binding.pry
+    @article = Article.new(article_params)
+    binding.pry
     @article.save
+  end
+private
+  def article_params
+    # params[:カラム名]
+    params.permit(:title, :content)
   end
 end
