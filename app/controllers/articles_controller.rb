8d6096204d5eca36973973508807ab106604234f
田口　勝貴 <d0811tgc@gmail.com>
2024-05-13 23:28:34 +0900

    1.プライベートメソッド ・インスタンス定義文作成 2.edit,update実装 3.showメソッド ・インスタンス定義文削除 4.before_action作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
   def show
-    @article = Article.find(params[:id])
+    binding.pry
+    # @article = Article.find(params[:id])
+    binding.pry
   end
   def new
     @article = Article.new
   end
   def create
     @article = Article.new(article_params)
-    @article
     if @article.save
       puts "保存に成功しました"
       # 保存成功時の処理
@@ -20,9 +22,25 @@ class ArticlesController < ApplicationController
     end
   end
   def edit
-    @article = Article.find_by(id: params[:id])
+    # binding.pry
+    # @article = Article.find_by(id: params[:id])
+    # binding.pry
+  end
+  def update
+    binding.pry
+    if @article.update(article_params)
+      binding.pry
+      puts "保存に成功しました"
+      # 保存成功時の処理
+        else
+       puts "保存に失敗しました"
+          # 保存失敗時の処理
+       end
   end
 private
+  def set_article
+    @article = Article.find_by(id: params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :content)
   end
