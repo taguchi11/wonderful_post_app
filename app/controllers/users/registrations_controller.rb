241dd687ea7b09cbb3afd7be0fbd34f1d75bb360
田口　勝貴 <d0811tgc@gmail.com>
2024-06-07 23:15:18 +0900

    railsデバイス設定 ・ルーティング編集 　devise_for :users, controllers: { 　　registrations:  'users/registrations' 　} ・ja.yml編集 　userモデル追加 ・registrationコントローラー 　newアクションコメント外し

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
   # def create
@@ -37,7 +37,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -49,11 +48,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
-
+  # # 入力フォームからアカウント名の情報をDB保存する
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
-  #   super(resource)
+  #   "/user/#{current_user.id}"
   # end
+
+  # サインイン
+  # def after_sign_in_path_for(resource)
+  #   "/user/#{current_user.id}"
+  # end
+
+  protected
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations:  'users/registrations'
+  }
   resources :articles
   resources :sample_articles
   root to:'articles#index'
