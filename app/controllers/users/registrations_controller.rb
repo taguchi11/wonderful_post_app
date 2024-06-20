44514a8a9041a951d3a2def9bdb03711b71ec595
田口　勝貴 <d0811tgc@gmail.com>
2024-06-21 00:07:17 +0900

    ルーティング変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,9 +7,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # GET /resource/sign_up
   def new
   # super
-  binding.pry
+  # binding.pry
     @user = User.new
-  binding.pry
+  # binding.pry
   end
 
   # POST /resource
